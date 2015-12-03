c166bbb04cc88942d29203475266ef46c75c6154
Jonny Arnold <jonny.arnold@reevoo.com>
2015-12-03 18:28:50 +0000

    Implemented arithmetic operations and boolean operators

--- runtime/default_scope.go
+++ runtime/default_scope.go
@@ -22,10 +22,40 @@ func (scope defaultScope) Call(args []fnScope) (fnScope, error) {
 
 var DefaultScope = defaultScope{
 	definitions: defMap{
-		"+": fn([]string{"a", "b"}, add),
+		"+":   fn([]string{"a", "b"}, add),
+		"-":   fn([]string{"a", "b"}, subtract),
+		"*":   fn([]string{"a", "b"}, multiply),
+		"/":   fn([]string{"a", "b"}, divide),
+		"and": fn([]string{"a", "b"}, and),
+		"or":  fn([]string{"a", "b"}, or),
+		"not": fn([]string{"a"}, not),
 	},
 }
 
 func add(args []fnScope) (fnScope, error) {
 	return NumberFromFloat(args[0].(number).AsFloat() + args[1].(number).AsFloat()), nil
+}
+
+func subtract(args []fnScope) (fnScope, error) {
+	return NumberFromFloat(args[0].(number).AsFloat() - args[1].(number).AsFloat()), nil
+}
+
+func multiply(args []fnScope) (fnScope, error) {
+	return NumberFromFloat(args[0].(number).AsFloat() * args[1].(number).AsFloat()), nil
+}
+
+func divide(args []fnScope) (fnScope, error) {
+	return NumberFromFloat(args[0].(number).AsFloat() / args[1].(number).AsFloat()), nil
+}
+
+func and(args []fnScope) (fnScope, error) {
+	return FnBool(args[0].(fnBool).value && args[1].(fnBool).value), nil
+}
+
+func or(args []fnScope) (fnScope, error) {
+	return FnBool(args[0].(fnBool).value || args[1].(fnBool).value), nil
+}
+
+func not(args []fnScope) (fnScope, error) {
+	return FnBool(!args[0].(fnBool).value), nil
 }
