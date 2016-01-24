e2fcf16e7af96e96431ced409e6a5e31cfcd50fc
Jonny Arnold <jonny.arnold89@gmail.com>
2016-01-24 10:42:49 +0000

    main.go will now parse the filename in args

--- main.go
+++ main.go
@@ -1,5 +1,36 @@
 package main
 
+import (
+	"fmt"
+	. "github.com/jonnyarnold/fn-go/parser"
+	. "github.com/jonnyarnold/fn-go/runtime"
+	. "github.com/jonnyarnold/fn-go/tokeniser"
+	"io/ioutil"
+	"os"
+)
+
 func main() {
-	tour()
+	file, _ := ioutil.ReadFile(os.Args[1])
+
+	tokens := Tokenise(string(file))
+
+	// for _, token := range tokens {
+	// 	fmt.Println(token)
+	// }
+
+	expressions, errors := Parse(tokens)
+
+	if errors != nil {
+		fmt.Println(errors)
+	}
+
+	// for _, expr := range expressions {
+	// 	fmt.Println(expr)
+	// }
+
+	result := Execute(expressions)
+
+	if result.Error != nil {
+		fmt.Println(result.Error)
+	}
 }
